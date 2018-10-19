5597ea92622dc8404991050972d39499f6d2825b
Ravera <laurent.ravera@irap.omp.eu>
2018-10-19 10:07:04 +0200

    Changes to have a "main" higher level routine: - Added process_demux_proto_tests.py as the highest level routine. - Update of analyse_demux_perfs.py script accordingly.

--- analyse_demux_perfs.py
+++ analyse_demux_perfs.py
@@ -1,32 +1,9 @@
-from DEMUX_TBox.general_tools import get_conf_csv, print_conf
-from DEMUX_TBox.gbw import process_GBW
-from DEMUX_TBox.dumps import process_dump, processIQ_multi
-from DEMUX_TBox.energy_resol import meas_energy_r
-import os
+#
+# This is a python script to be used to process the data of specific test sequences.
+# 
 
-config=get_conf_csv()
+from DEMUX_TBox.process_demux_proto_tests import process_demux_proto_tests
+
 dirname = '20181017_091738'
 
-
-fulldirname = os.path.join(config['path_tests'], dirname)
-
-# -----------------------------------------------------------------------
-# Processing "BIAS, FEEDBAC and INPUT" dump files 
-process_dump(fulldirname, config, Max_duration=1.0)
-
-# -----------------------------------------------------------------------
-# Processing "Gain bandwidth characterization" 
-chan=0
-dumptype = "IQ-ALL"
-process_GBW(fulldirname, dumptype, chan)
-dumptype = "IQ-TST"
-process_GBW(fulldirname, dumptype, chan)
-
-# -----------------------------------------------------------------------
-# Processing "Carriers spectra characterization"
-pix=40 # test pixel
-spt0dB = processIQ_multi(fulldirname, config, pix_zoom=pix, SR=False)
-
-# -----------------------------------------------------------------------
-# Processing "Energy resolution characterization"
-meas_energy_r(fulldirname)
+process_demux_proto_tests(dirname)
---
+++ process_demux_proto_tests.py
@@ -0,0 +1,34 @@
+from DEMUX_TBox.general_tools import get_conf_csv, print_conf
+from DEMUX_TBox.gbw import process_GBW
+from DEMUX_TBox.dumps import process_dump, processIQ_multi
+from DEMUX_TBox.energy_resol import meas_energy_r
+import os
+
+# ---------------------------------------------------------------------------
+def process_demux_proto_tests(dirname):
+    config=get_conf_csv()
+
+    fulldirname = os.path.join(config['path_tests'], dirname)
+
+    # -----------------------------------------------------------------------
+    # Processing "BIAS, FEEDBAC and INPUT" dump files 
+    process_dump(fulldirname, config, Max_duration=1.0)
+
+    # -----------------------------------------------------------------------
+    # Processing "Gain bandwidth characterization" 
+    chan=0
+    dumptype = "IQ-ALL"
+    process_GBW(fulldirname, dumptype, chan)
+    dumptype = "IQ-TST"
+    process_GBW(fulldirname, dumptype, chan)
+
+    # -----------------------------------------------------------------------
+    # Processing "Carriers spectra characterization"
+    pix=40 # test pixel index
+    spt0dB = processIQ_multi(fulldirname, config, pix_zoom=pix, SR=False)
+
+    # -----------------------------------------------------------------------
+    # Processing "Energy resolution characterization"
+    meas_energy_r(fulldirname)
+
+# ---------------------------------------------------------------------------

9215cc8c20f6ae8b445d9960d7f6fe003416433e
Ravera <laurent.ravera@irap.omp.eu>
2018-10-21 17:57:51 +0200

    demux_tools_cfg.csv updated

--- analyse_demux_perfs.py
+++ analyse_demux_perfs.py
@@ -4,6 +4,7 @@
 
 from DEMUX_TBox.process_demux_proto_tests import process_demux_proto_tests
 
-dirname = '20181017_091738'
+#dirname = '20181017_091738'
+dirname = '20181019_160619'
 
 process_demux_proto_tests(dirname)
